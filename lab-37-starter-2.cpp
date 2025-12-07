23b1b1e82a40d400c9ce5ae79aba56514eb71126
Aditya Patra <adityabpatra@gmail.com>
2025-12-06 17:32:30 -0800

    test successful, able to delete and add key

--- lab-37-starter-2.cpp
+++ lab-37-starter-2.cpp
@@ -170,6 +170,6 @@ int gen_hash_index(string key) {
 These targets are present in the dataset and can be used for testing:
 536B9DFC93AF
 1DA9D64D02A0 
-666D109AA22E
+666D109AA22E 
 E1D2665B21EA
 */
